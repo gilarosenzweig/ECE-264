2a2065d1aa78db9143da0775e81f2c5d7814352a
GRosenzweig <rosenzweiggila@gmail.com>
2021-04-16 16:09:47 -0400

    an original commit message

--- main.cpp
+++ main.cpp
@@ -76,7 +76,7 @@ public:
         else {
             insert -> setNext(firstptr);
             firstptr = insert;
-            length = length +1;
+            length = length + 1;
         }
     }
     void insertAtEnd(stuff thing){
@@ -100,45 +100,38 @@ public:
             stuff extract = firstptr -> getValue();
             firstptr = nullptr;
             lastptr = nullptr;
-            length = length -1;
+            length = length - 1;
             return extract;
         }
         else {
             stuff extract = firstptr -> getValue();
             firstptr = firstptr -> getNext();
-            length = length -1;
+            length = length - 1;
             return extract;
         }
     }
 };
 
-template <typename stuff> class stack {
-
-private:
-    string name;
-public:
-    stack(const string &name) : name(name) {
-    }
-    void push(stuff thing){
-        this -> insertAtStart(thing);
-    }
-    stuff pop(){
-        return this -> removeAtStart();
-    }
+template <typename stuff> class stack: public simpleList<stuff> {
+    using simpleList<stuff>::simpleList;
+    public:
+        void push(stuff thing){
+            this -> insertAtStart(thing);
+        }
+        stuff pop(){
+            return this -> removeAtStart();
+        }
 };
 
-template <typename stuff> class queue {
-private:
-    string name;
-public:
-    queue(const string &name) : name(name) {
-    }
-    void push(stuff thing){
-        this -> insertAtEnd(thing);
-    }
-    stuff pop() {
-        return this -> removeAtStart();
-    }
+template <typename stuff> class queue: public simpleList<stuff>{
+    using simpleList<stuff>::simpleList;
+    public:
+        void push(stuff thing){
+            this -> insertAtEnd(thing);
+        }
+        stuff pop() {
+            return this -> removeAtStart();
+        }
 };
 
 bool checkName(string name, list<string> nameList){
@@ -160,12 +153,12 @@ void parse(string words[], ifstream &inputfile){
         inputfile >> words[2];
     }
 }
-template <typename T> SimpleList<T>* getList(string name, list<SimpleList<T>*> setOfLists) {
-for (auto it = setOfLists.begin(); it != setOfLists.end(); it++) {
-if (((*it) -> getName()) == name)
-return *it;
-}
-return nullptr;
+template <typename T> simpleList<T>* getList(string name, list<simpleList<T>*> setOfLists) {
+    for (auto it = setOfLists.begin(); it != setOfLists.end(); it++) {
+        if (((*it) -> getName()) == name)
+            return *it;
+    }
+    return nullptr;
 }
 
 int main() {
@@ -183,40 +176,74 @@ int main() {
             parse(words, inputfile);
             outputfile << "PROCESSING COMMAND: " << words[0] << " " << words[1] << " " << words[2] << "\n";
             char dataType = words[1].at(0);
-            //cout << dataType;
             if (words[0] == "create"){
                 if (checkName(words[1], nameList)){
                     outputfile << "ERROR: This name already exists!\n";
+                    continue;
                 }
                 nameList.push_front(words[1]);
                 if (words[2] == "stack"){
                     if (dataType == 'i'){
                         //push new stack into listSLi
+                        listSLi.push_front(new stack<int> (words[1]));
                     }
                     else if (dataType == 'd'){
                         //push new stack into listSLd
+                        listSLd.push_front(new stack<double> (words[1]));
                     }
                     else if (dataType == 's'){
                         //push new stack into listSLs
+                        listSLs.push_front(new stack<string> (words[1]));
                     }
                 }
                 else if (words[2] == "queue"){
                     if (dataType == 'i'){
                         //push new queue into listSLi
-                        new queue<int> (words[1]);
+                        listSLi.push_front(new queue<int> (words[1]));
                     }
                     else if (dataType == 'd'){
                         //push new queue into SLd
+                        listSLd.push_front(new queue<double> (words[1]));
                     }
                     else if (dataType == 's'){
                         // push new queue into listSLs
+                        listSLs.push_front(new queue<string> (words[1]));
                     }
                 }
             }
             else if (words[0] == "push"){
-
+                if ( !checkName(words[1], nameList)){
+                    outputfile << "ERROR: This name does not exist!\n";
+                    continue;
+                }
+                if (dataType =='i'){
+                    getList(words[1], listSLi) -> push(stoi(words[2]));
+                }
+                else if (dataType == 'd'){
+                    getList(words[1], listSLd) -> push(stod(words[2]));
+                }
+                else if (dataType == 's'){
+                    getList(words[1], listSLs) -> push(words[2]);
+                }
             }
             else if (words[0] == "pop"){
+                if ( !checkName(words[1], nameList)){
+                    outputfile << "ERROR: This name does not exist!\n";
+                    continue;
+                }
+                switch (dataType) {
+                    case 'i':
+                        getList(words[1], listSLi) -> pop();
+                        break;
+                    case 'd':
+                        getList(words[1], listSLd) -> pop();
+                        break;
+                    case 's':
+                        getList(words[1], listSLs) -> pop();
+                        break;
+                    default:
+                        break;
+                }
 
             }
         }
